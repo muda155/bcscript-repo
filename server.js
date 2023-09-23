f5d272ec8707dd7c5032a6e19fd6afabd8e76531
Dương Mạc <macduong@Duongs-MacBook-Air.local>
2023-09-23 16:27:40 +0700

    Hash and Compare Passwords Asynchronously

--- server.js
+++ server.js
@@ -12,12 +12,15 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    console.log(hash);
+});
 
 
 //END_ASYNC
 
 //START_SYNC
+
 
 
 
